d18aea439eaf5a2892a23978671edf00c08c0717
Blake Jones <blakemj@stanford.edu>
2018-12-06 22:48:09 -0800

    Working on fixing small things

--- Ms_Pacman/main.c
+++ Ms_Pacman/main.c
@@ -10,12 +10,29 @@
 #include "strings.h"
 #include "pacman_printf.h"
 
+//
 #define BOARD_WIDTH 224
 #define BOARD_HEIGHT 288
+#define GAME_OVER_DELAY 5
+#define NUM_SUPER_DOTS 4
+#define TOTAL_DOTS 244
+#define START_DELAY 3
+#define INIT_NUM_LIVES 3
+#define EXTRA_LIFE_SCORE 1000
+#define TILE_WIDTH 8
+#define NUM_SCORE_CHAR 9
 
+//
 static int charWidth;
 static int charHeight;
 
+//
+static int lives;
+static int extraLife;
+
+/*
+*
+*/
 static void start_screen() {
     char* title = pacman_printf("MS. PACMAN");
     gl_draw_string(BOARD_WIDTH / 2 - charWidth * (strlen(title) / 2), charHeight / 2, title, GL_YELLOW);
@@ -31,93 +48,160 @@ static void start_screen() {
     gl_draw_string(BOARD_WIDTH / 2 - charWidth * (strlen(twoPlay) / 2), BOARD_HEIGHT / 2 - charHeight / 2, twoPlay, GL_AMBER);
 }
 
+/*
+*
+*/
 static void game_over() {
     char* gameOver = pacman_printf("GAME OVER");
     gl_draw_string(BOARD_WIDTH / 2 - charWidth * (strlen(gameOver) / 2), BOARD_HEIGHT / 2 - charHeight / 2, gameOver, GL_RED);
     gl_swap_buffer();
 }
 
+/*
+*
+*/
 static int divide(int numTens, int divider) {
-    for (int i = 0; i < 8 - numTens; i++) {
+    for (int i = 0; i < NUM_SCORE_CHAR - 1 - numTens; i++) {
         divider = divider / 10;
     }
     return divider;
 }
 
-static int total_score;
+/*
+*
+*/
+static void erase_everything(int twoPlayer) {
+    erase_blinky();
+    erase_pinky();
+    erase_inky();
+    erase_clyde();
+    erase_pacman(pacman_get_x(), pacman_get_y());
+    if (twoPlayer) erase_pacman(ms_pacman_get_x(), ms_pacman_get_y());
+}
 
+/*
+*
+*/
+static int determine_player_mode(int twoPlayer) {
+    read_nes_controller();
+    if (!a_button) twoPlayer = 0;
+    if (!b_button) twoPlayer = 1;
+    if (!twoPlayer) {
+        gl_draw_rect(0, BOARD_HEIGHT / 2 - TILE_WIDTH / 2, TILE_WIDTH, TILE_WIDTH, GL_BLACK);
+        gl_draw_rect(0, BOARD_HEIGHT / 2 - 3 * charHeight / 2 - TILE_WIDTH / 2, TILE_WIDTH, TILE_WIDTH, GL_PURPLE1);
+    } else {
+        gl_draw_rect(0, BOARD_HEIGHT / 2 - 3 * charHeight / 2 - TILE_WIDTH / 2, TILE_WIDTH, TILE_WIDTH, GL_BLACK);
+        gl_draw_rect(0, BOARD_HEIGHT / 2 - TILE_WIDTH / 2, TILE_WIDTH, TILE_WIDTH, GL_PURPLE1);
+    }
+    gl_swap_buffer();
+    return twoPlayer;
+}
+
+/*
+*
+*/
+static void move_characters(int twoPlayer) {
+    if(!pacman_hit_ghost()) pacman_move();
+    if (twoPlayer && !ms_pacman_hit_ghost()) ms_pacman_move();
+    blinky_move();
+    pinky_move();
+    inky_move();
+    clyde_move();
+}
+
+/*
+*
+*/
+static void display_lives() {
+    gl_draw_rect(0, 34*TILE_WIDTH, gl_get_width(), 2*TILE_WIDTH, GL_BLACK);
+    for (int i = 0; i < lives - 1; i++) {
+        draw_pacman(i*(2*TILE_WIDTH) + 1, 34*TILE_WIDTH, pacman_get_curMove());
+    }
+    gl_swap_buffer();
+}
+
+/*
+*
+*/
+static int update_score(int prevScore) {
+    gl_draw_rect(TILE_WIDTH, 1, NUM_SCORE_CHAR*charWidth, charHeight, GL_BLACK);
+    if (frightenedPointsNotAdded) {
+        prevScore = prevScore + frightened_points;
+        frightenedPointsNotAdded = 0;
+    }
+    int total_score = prevScore + TOTAL_DOTS - numDots + superDotBonus;
+    for (int i = 0; i < NUM_SCORE_CHAR; i++) {
+        gl_draw_char(i*charWidth + TILE_WIDTH, 1, divide(i, total_score) % 10 + '0', GL_WHITE);
+    }
+    if (total_score > extraLife * EXTRA_LIFE_SCORE) {
+        lives++;
+        extraLife++;
+        display_lives();
+    }
+    gl_swap_buffer();
+    return prevScore;
+}
+
+/*
+*
+*/
+static int update_while_still_alive(int prevScore, int twoPlayer) {
+    move_characters(twoPlayer);
+    prevScore = update_score(prevScore);
+    if(pacman_hit_ghost()) erase_pacman(pacman_get_x(), pacman_get_y());
+    if(twoPlayer && ms_pacman_hit_ghost()) erase_pacman(ms_pacman_get_x(), ms_pacman_get_y());
+    return prevScore;
+} 
+
+/*
+*
+*/
+static int reset_between_lives(int prevScore, int twoPlayer) {
+    erase_everything(twoPlayer);
+    if (pacman_hit_ghost() && ms_pacman_hit_ghost()) lives--;
+    prevScore = prevScore + superDotBonus;
+    if (!(numDots - NUM_SUPER_DOTS)) {
+        draw_dots();
+        prevScore = prevScore + TOTAL_DOTS;
+    }
+    return prevScore;
+}
+
+/*
+*
+*/
 void main(void) 
 {
   charWidth = gl_get_char_width();
   charHeight = gl_get_char_height();
   gl_init(BOARD_WIDTH, BOARD_HEIGHT, GL_DOUBLEBUFFER);
   while(1) {
-      int lives = 3;
-      int frightenedBonus = 0;
+      lives = INIT_NUM_LIVES;
       nes_init();
       gl_clear(GL_BLACK);
       start_screen();
       start_button = 1;
       int twoPlayer = 0;
       while (start_button) {
-          read_nes_controller();
-          if (!a_button) twoPlayer = 0;
-          if (!b_button) twoPlayer = 1;
-          if (!twoPlayer) {
-              gl_draw_rect(0, BOARD_HEIGHT / 2 - 4, 8, 8, GL_BLACK);
-              gl_draw_rect(0, BOARD_HEIGHT / 2 - 3 * charHeight / 2 - 4, 8, 8, GL_PURPLE1);
-          } else {
-              gl_draw_rect(0, BOARD_HEIGHT / 2 - 3 * charHeight / 2 - 4, 8, 8, GL_BLACK);
-              gl_draw_rect(0, BOARD_HEIGHT / 2 - 4, 8, 8, GL_PURPLE1);
-          }
-          gl_swap_buffer();
+          twoPlayer = determine_player_mode(twoPlayer);
       }
       board_init();
       draw_dots();
       int prevScore = 0;
+      extraLife = 1;
       while (lives) {
           pacman_init();
           if (twoPlayer) ms_pacman_init();
           ghosts_init();
-          for (int i = 0; i < lives - 1; i++) {
-              draw_pacman(i*(2*8) + 1, 34*8, pacman_get_curMove());
+          display_lives();
+          timer_delay(START_DELAY);
+          while(numDots - NUM_SUPER_DOTS && !(pacman_hit_ghost() && ms_pacman_hit_ghost())) {
+              prevScore = update_while_still_alive(prevScore, twoPlayer);
           }
-          gl_swap_buffer();
-          timer_delay(3);
-          while(numDots - 4 && !(pacman_hit_ghost() && ms_pacman_hit_ghost())) {
-              if(!pacman_hit_ghost()) pacman_move();
-              if (twoPlayer && !ms_pacman_hit_ghost()) ms_pacman_move();
-              blinky_move();
-              pinky_move();
-              inky_move();
-              clyde_move();
-              if (frightenedPointsNotAdded) {
-                  frightenedBonus = frightenedBonus + frightened_points;
-                  frightenedPointsNotAdded = 0;
-              }
-              total_score = prevScore + 244 - numDots + superDotBonus + frightenedBonus;
-              if (total_score > 10000) lives++;
-              for (int i = 0; i < 9; i++) {
-                  gl_draw_char(i*charWidth + 8, 1, divide(i, total_score) % 10 + 48, GL_WHITE);
-              }
-              gl_swap_buffer();
-              gl_draw_rect(8, 1, 9*charWidth, charHeight, GL_BLACK);
-              if(pacman_hit_ghost()) erase_pacman(pacman_get_x(), pacman_get_y());
-              if(twoPlayer && ms_pacman_hit_ghost()) erase_pacman(ms_pacman_get_x(), ms_pacman_get_y());
-          }
-          erase_blinky();
-          erase_pinky();
-          erase_inky();
-          erase_clyde();
-          erase_pacman(pacman_get_x(), pacman_get_y());
-          erase_pacman(ms_pacman_get_x(), ms_pacman_get_y());
-          prevScore = total_score;
-          if (pacman_hit_ghost() && ms_pacman_hit_ghost()) lives--;
-          if (!(numDots - 4)) draw_dots();
-          gl_draw_rect(0, 34*8, gl_get_width(), 2*8, GL_BLACK);
+          prevScore = reset_between_lives(prevScore, twoPlayer);
       }
       game_over();
-      timer_delay(5);
+      timer_delay(GAME_OVER_DELAY);
   }
 }
 
